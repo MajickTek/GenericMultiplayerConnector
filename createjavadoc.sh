3d50eb2941e103b99ca7668a1ef94a32712a0a0f
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2016-10-18 21:35:33 +0100

    Added javadocs

---
+++ createjavadoc.sh
@@ -0,0 +1,2 @@
+javadoc -d javadocs -sourcepath ./GenericMultiplayerClient/src -subpackages java com.scs 
+#javadoc com.scs -d javadocs -sourcepath ./GenericMultiplayerClient/src;./GenericMultiplayerServer/src;./GenericMultiplayerShared/src -subpackages com.scs
